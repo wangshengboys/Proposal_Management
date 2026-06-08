053570bf7c9e89ba5608973099515e9abcbcf8f4
Your Name <you@example.com>
2026-06-08 21:53:13 +0700

    LoginPage

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // <-- Tambahkan baris ini
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), // <-- Tambahkan juga di sini
+  ],
 })
